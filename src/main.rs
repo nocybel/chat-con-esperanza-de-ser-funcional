504adf8d2e1cc33239b382768a020be94e9f2f25
Cybel <cybell@ciencias.unam.mx>
2026-08-31 12:57:53 -0600

    mini avance, estoy intentando filtrar el tipo de mensaje recibido del servidor

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+mod procesador;
+
 fn main() {
+    procesador::recibir_mensaje(
+        &r#"{ "type": "USER_LIST",
+              "users": { "Kimberly": "ACTIVE",
+              "Luis": "BUSY",
+              "Fernando": "AWAY",
+              "Antonio": "ACTIVE" } }"#.to_string()
+            );
     println!("Hola soy el cliente :D!");
 }
--- src/procesador.rs
+++ src/procesador.rs
@@ -1,4 +1,5 @@
-//use serde_json;
+//use serde::{Deserialize, Serialize};
+use serde_json::{Result, Value};
 
 /*
  * Archivo procesador.
@@ -6,23 +7,15 @@
  * Tambien debería poder formatear Strings crudos para mandarlos por la antena.
  */
 
-fn recibirMensaje(mensaje: &String) {
-    struct Info {
-        r#type: String,
-        username: String,
-        status: String,
-        users: Vec<String>,
-        text: String,
-        roomname: String,
-    };
-
+pub fn recibir_mensaje(mensaje: &String) -> Result<()> {
     // falta poner que serde_json ponga los cosos donde van
     // rust da miedo unu
 
+    let v: Value = serde_json::from_str(mensaje)?;
+    let t: String = v["type"].to_string().;
+    println!("{}", t);
 
-    let tipo: str = "aqui iría el tipo del mensaje jeje";
-
-    match tipo {
+    match t.as_str() {
         "NEW_USER" => msg_new_user(),
         "NEW_STATUS" => msg_new_status(),
         "USER_LIST" => msg_user_list(),
@@ -34,47 +27,48 @@ fn recibirMensaje(mensaje: &String) {
         "DISCONNECTED" => msg_disconnected(),
         other => println!("hola"),
     }
+    Ok(())
 }
 
 
 // Imaginemos que hay un private antes de todos los metodos msg_algo(), y que nadie nunca jamas los va a usar mas que en la funcion recibirMensaje()
 
 fn msg_new_user() {
-
+    println!("alguien se unio al servidor");
 }
 
 fn msg_new_status() {
-
+    println!("alguien cambio de status");
 }
 
 fn msg_user_list() {
-
+    println!("lista de usuarios");
 }
 
 fn msg_text_from() {
-
+    println!("alguien escribio texto privado");
 }
 
 fn msg_public_text_from() {
-
+    println!("alguien escribio texto publico");
 }
 
 fn msg_joined_room() {
-
+    println!("alguien se unio a la sala");
 }
 
 fn msg_room_user_list() {
-
+    println!("lista de usuarios de una sala");
 }
 
 fn msg_room_text_from() {
-
+    println!("alguien escribio texto en una sala");
 }
 
 fn msg_left_room() {
-
+    println!("alguien se salio de la sala");
 }
 
 fn msg_disconnected() {
-
+    println!("alguien se desconecto");
 }
